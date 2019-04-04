00c608b93ad8e2f724860e506b0ec14bda815423
Noah Braam <nwbraam@gmail.com>
2019-04-04 18:03:35 -0400

    start a4

---
+++ a4/spigot.adb
@@ -0,0 +1,14 @@
+with ada.Text_IO; use Ada.Text_IO;
+with ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+
+procedure spigot is
+    n, len, i, j, k, q, x, nines, predigit : Integer;
+    a : array (1..1000) of Long_Integer;
+begin
+    n:=1000;
+    a(1..1000):=2;
+    nines:=0;
+    predigit:=0;
+    for j in 1..n loop
+        q:=0;
+    end loop;
